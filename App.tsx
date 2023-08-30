cfe681ed1e1da64684c7d09bdb7fdc268e190361
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2023-08-29 22:19:44 -0400

    moving from next to react

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import GroupsSidebar from "./components/groupsSidebar";
+import ServiceDashboard from "./components/serviceDashboard";
+import ServicesSidebar from "./components/servicesSidebar";
 
 export default function App() {
   return (
     <>
-      TEST FROM APP.TSX
-      {/* <GroupSidebar />
-      <ServiceSidebar />
-      <ServiceDashboard /> */}
+      <GroupsSidebar />
+      <ServicesSidebar />
+      <ServiceDashboard />
     </>)
 }
