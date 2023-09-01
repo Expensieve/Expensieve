d0ea105069be237f553460e12f279f50c02ebf2b
Lucas Contreras <32287834+lucascontreras@users.noreply.github.com>
2023-08-31 21:15:39 -0400

    resolved errors after merge

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GroupsSidebar from "./src/front-end/components/groupsSidebar";
 import ServicesSidebar from "./src/front-end/components/servicesSidebar";
-import ServiceDashboard from "./src/front-end/components/serviceDashboard";
+import ServiceDashboard from "./src/front-end/components/ServiceDashboard";
 import './app.css';
 
 export default function App() {
