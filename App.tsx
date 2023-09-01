f466f0a486b0e9949d9626075318c3bfd7bb0f04
mbildstein <michael.bildstein@gmail.com>
2023-08-31 19:41:39 -0700

    webpack error resolved

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import GroupSidebar from "./src/front-end/components/GroupSidebar";
 import ServiceSidebar from "./src/front-end/components/ServiceSidebar";
 import ServiceDashboard from "./src/front-end/components/ServiceDashboard";
-
-const [groupSelection, setGroupSelection] = useState("");
-const [serviceSelection, setServiceSelection] = useState("");
+import "./app.css"
 
 export default function App() {
+  const [groupSelection, setGroupSelection] = useState("");
+  const [serviceSelection, setServiceSelection] = useState("");
+
   return (
     <>
       <GroupSidebar groupSelection = {groupSelection} setGroupSelection = {setGroupSelection}/>
