630f05247095b01fb48c0370926758cb31402e24
John Nevarez <nevarezjonathan11@gmail.com>
2023-09-02 16:19:43 -0400

    Added group assignment to service creation and installed sass dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
   module: {
     rules:[
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: "babel-loader"
